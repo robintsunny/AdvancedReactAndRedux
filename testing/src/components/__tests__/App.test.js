633318aa2253c1b8a08d3da43065221e398033c0
robin <robintsunny@gmail.com>
2018-08-23 16:11:22 -0700

    Redux Setup

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -4,19 +4,20 @@ import App from 'components/App'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
 
+let wrapped
+
 beforeEach (() => {
-  var wrapped = shallow(<App />)
+  wrapped = shallow(<App />)
 })
 
-it('shows a comment box', () => {
 
+
+
+it('shows a comment box', () => {
   expect(wrapped.find(CommentBox).length).toEqual(1)
-  
 })
 
 
 it('shows a comment list', () => {
-
   expect(wrapped.find(CommentList).length).toEqual(1)
-
 })
---
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {mount} from 'enzyme'
+import CommentBox from 'components/Commentbox'
+
+let wrapped;
+
+beforeEach (() => {
+    wrapped = mount(<CommentBox />)
+})
+
+afterEach(() => {
+    wrapped.unmount()
+})
+
+
+
+
+it('has a text area and a button', () => {
+    expect(wrapped.find('textarea').length).toEqual(1)
+    expect(wrapped.find('button').length).toEqual(1)
+})
+
+
+describe('the text area', () => {
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: { value: 'new comment'}
+        })
+        wrapped.update()
+    })
+
+    it('has a text area that users can type in', () => {
+        // wrapped.find('textarea').simulate('change', {
+        //     target: { value: 'new comment'}
+        // })
+        // wrapped.update()
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+    })
+    
+    
+    
+    it('when form is submitted, the text area is emptied', () => {
+        // wrapped.find('textarea').simulate('change', {
+        //     target: { value: 'new comment'}
+        // })
+        // wrapped.update()
+        
+        wrapped.find('form').simulate('submit')
+        expect(wrapped.find('textarea').prop('value')).toEqual('')
+    })
+})
