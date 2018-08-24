fccac6325ab207e38bce4eeab242209f0c9f5d4c
robin <robintsunny@gmail.com>
2018-08-23 18:26:33 -0700

    Cheerio Queries

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -5,7 +5,7 @@ import reducers from 'reducers'
 
 export default (props) => {
     return (
-        <Provider store={createStore(reducers, {})}>
+        <Provider store={createStore(reducers, props.initialState)}>
             {props.children}
         </Provider>
     )
