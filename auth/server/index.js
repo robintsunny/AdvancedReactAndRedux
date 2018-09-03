2167c0a671c5f54430f6ba21abd05b4aade4b6e3
robin <robintsunny@gmail.com>
2018-09-03 16:24:39 -0500

    Mongo User Model

---
+++ auth/server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const http = require('http')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+
+const app = express();
+const router = require('./router')
+
+
+// APP SETUP
+
+app.use(morgan('combined'))
+app.use(bodyParser.json({type: '*/*'}))
+router(app)
+
+// SERVER SETUP
+const port = process.env.PORT || 3090;
+const server = http.createServer(app)
+server.listen(port)
+console.log('server listening on port', port)
