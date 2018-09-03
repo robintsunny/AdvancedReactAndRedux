1130cf42cbc6ab708750c16deda1be0e3bd7d429
robin <robintsunny@gmail.com>
2018-09-03 16:55:06 -0500

    RoboMongo Installation

--- auth/server/index.js
+++ auth/server/index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const http = require('http')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-
-
 const app = express();
 const router = require('./router')
+const mongoose = require('mongoose')
 
+
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth ')
 
 // APP SETUP
 
